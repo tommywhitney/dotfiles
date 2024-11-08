258fea7a9ec29d1e4d18e97a5936d40e0355eb0c
Tommy Whitney <46704616+tommywhitney@users.noreply.github.com>
2024-11-08 09:23:33 -0500

    aerospace, nvim, tmux configs

---
+++ nvim/lua/plugins/lsp-config.lua
@@ -0,0 +1,110 @@
+return {
+  {
+    "williamboman/mason.nvim",
+    config = function()
+      require("mason").setup()
+    end,
+  },
+  {
+    "williamboman/mason-lspconfig.nvim",
+    config = function()
+      require("mason-lspconfig").setup({
+        ensure_installed = {
+          "lua_ls",
+          "html",
+          "cssls",
+          "tsserver",
+          "svelte",
+          "tailwindcss",
+          "emmet_language_server",
+          "eslint",
+          "pyright",
+          "ruff",
+        },
+      })
+    end,
+  },
+  {
+    "neovim/nvim-lspconfig",
+    config = function()
+      local capabilities = require("cmp_nvim_lsp").default_capabilities()
+
+      local lspconfig = require("lspconfig")
+      lspconfig.lua_ls.setup({
+        capabilities = capabilities,
+      })
+      lspconfig.html.setup({
+        capabilities = capabilities,
+      })
+      lspconfig.cssls.setup({
+        capabilities = capabilities,
+      })
+      lspconfig.tsserver.setup({
+        capabilities = capabilities,
+      })
+      lspconfig.pyright.setup({
+        capabilities = capabilities,
+        init_options = {
+          settings = {
+            pyright = {
+              settings = {
+                pyright = {
+                  disableOrganizeImports = true, -- Using Ruff
+                },
+                python = {
+                  analysis = {
+                    ignore = { "*" }, -- Using Ruff
+                  },
+                },
+              },
+            },
+          },
+        },
+      })
+      lspconfig.ruff.setup({
+        capabilities = capabilities,
+        init_options = {
+          settings = {
+            args = {
+              "--line-length=120",
+              "--indent-width=4",
+            },
+          },
+        },
+      })
+      lspconfig.svelte.setup({
+        capabilities = capabilities,
+      })
+      lspconfig.tailwindcss.setup({
+        capabilities = capabilities,
+      })
+      lspconfig.eslint.setup({
+        capabilities = capabilities,
+      })
+      lspconfig.emmet_language_server.setup({
+        filetypes = {
+          "css",
+          "eruby",
+          "html",
+          "javascript",
+          "javascriptreact",
+          "less",
+          "sass",
+          "scss",
+          "pug",
+          "typescriptreact",
+          "svelte",
+        },
+        init_options = {
+          showAbbreviationSuggestions = true,
+          showExpandedAbbreviation = "always",
+          showSuggestionsAsSnippets = false,
+        },
+      })
+
+      vim.keymap.set("n", "K", vim.lsp.buf.hover, {})
+      vim.keymap.set("n", "<leader>gd", vim.lsp.buf.definition, {})
+      vim.keymap.set("n", "<leader>ca", vim.lsp.buf.code_action, {})
+    end,
+  },
+}

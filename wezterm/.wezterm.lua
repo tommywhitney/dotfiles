fec4792fb8a797efcac709f2b6b3cbd26ce482bd
Tommy Whitney <tommy.whitney7@gmail.com>
2025-08-19 13:29:18 -0400

    update config

--- wezterm/.wezterm.lua
+++ wezterm/.wezterm.lua
@@ -6,15 +6,16 @@ local config = wezterm.config_builder()
 
 -- This is where you actually apply your config choices
 
+config.color_scheme = "catppuccin-macchiato"
 config.font = wezterm.font("MesloLGS Nerd Font Mono")
-config.font_size = 16
+config.font_size = 14
+config.enable_tab_bar = false
 
--- config.enable_tab_bar = false
+config.window_decorations = "RESIZE"
 
--- config.window_decorations = "RESIZE"
-
-config.window_background_opacity = 0.8
+config.window_background_opacity = 0.85
 config.macos_window_background_blur = 10
 
 -- and finally, return the configuration to wezterm
 return config
+
